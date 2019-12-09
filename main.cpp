a95b3fc4347b39bfe1d1969523261bd5e8aac7a0
Klaus33 <claudiup340@gmail.com>
2019-12-09 20:41:51 +0200

    Add function struct type

--- main.cpp
+++ main.cpp
@@ -2,8 +2,26 @@
 #include "functions.h"
 using namespace std;
 
+/*
+* O functie este declarata sub forma unei structuri de tip inregistrare
+*  expresie = expresia functiei
+*  cuvinte = contine toate cuvintele ce formeaza expresia matematica
+*  lungime = lungimea efectiva a vectorului
+*  a,b = un interval de evaluare a functiei
+*  n = numarul de puncte de evaluare
+*/
+struct record {
+    char expresie[]; 
+    char cuvinte[255][20]; 
+    int lungime; 
+    float a,b; 
+    int n; 
+};
+
+typedef record functie;
+
 int main()
 {
-      
+    
     return 0;
 }
