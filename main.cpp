16c00cd2d5bac501b9f6175d92e29c405f76c7f7
unknown <roscavc@yahoo.com>
2020-01-06 18:36:37 +0200

    CalculatorVar1

--- main.cpp
+++ main.cpp
@@ -29,6 +29,7 @@ using namespace std;
 *  a,b = un interval de evaluare a functiei
 *  n = numarul de puncte de evaluare
 */
+
 struct record
 {
     char expresie[256];
@@ -653,19 +654,43 @@ void afisare(functie E)
     }
 }
 
+char c2[10000],k;
+
 int main()
 {
     int window1,i;
-    /*citesteFuntie(E);
+    char c[10000],result[100];
+
+    citesteFunctie(E);
     afiseazaCuvinte();
 
-    afisare(E);*/
-    window1=initwindow(600,600,"evaluatorMatematic",true,true);
-    char key,c[101];
+    afisare(E);
 
-    cin>>c;
 
-    cinTastatura(window1,c);
+   // window1=initwindow(600,600,"evaluatorMatematic",true,true);
+
+   /* cin>>k;
+
+    settextstyle(BOLD_FONT,HORIZ_DIR,2);
+    outtextxy(150,205,"Valoare Functie/f(x)");
+    rectangle(0,235,600,265);//Dreptunghi sub textul Valoare Functie/f(x)
+    for(i=50; i<=600; i=i+50)
+        line(i,235,i,265);
+
+    for(i=0;i<=11;i++)
+        citireX2(window1,k,i);*/
+    //cinTastatura(window1,c);
+
+    //citesteString(window1);
+
+    /*settextstyle(BOLD_FONT,HORIZ_DIR,2);
+    outtextxy(150,151,"Citeste Necunoscuta X");
+    rectangle(0,172,600,200);//Dreptunghide sub textul Citeste Necunoscuta X
+    for(i=50; i<=600; i=i+50)
+        line(i,172,i,200);*/
+
+
+    //citesteString(window1);
 
     getch();
     closegraph();
